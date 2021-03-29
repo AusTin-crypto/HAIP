9e99c7bf4dd02abdc1e22586e6a9f9fe2f932078
Austin <fernandesjoly@gmail.com>
2021-03-29 18:11:58 +0530

    Login

---
+++ app/src/main/java/com/example/control_majnu/RecruiterProfile.java
@@ -0,0 +1,14 @@
+package com.example.control_majnu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecruiterProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recruiter_profile);
+    }
+}
